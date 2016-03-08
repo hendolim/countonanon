2e352350df0389059605aa762f5729a83dc2d188
alo0822 <annielo@berkeley.edu>
2016-03-08 07:33:01 +0000

    Generated static home, about, and contact html pages (no styling)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  get 'about' => 'static_pages#about', :as => :home_about
+  get 'contact' => 'static_pages#contact', :as => :home_contact
+
 end
