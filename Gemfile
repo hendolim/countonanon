12b0af47feb346d708b0215c5cb21539a576da69
mnguyen7795 <mnguyen@berkeley.edu>
2016-03-09 04:30:43 +0000

    Created User Model with Devise allowing for Sign Up and Log In but not Log Out yet

--- Gemfile
+++ Gemfile
@@ -21,6 +21,11 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# SafeSpace Added Gems
+gem 'devise', '3.4.1'
+gem 'bootstrap-sass'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#home'
+  get 'about' => 'static_pages#about', :as => :home_about
+  get 'contact' => 'static_pages#contact', :as => :home_contact
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,8 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
-  get 'about' => 'static_pages#about', :as => :home_about
-  get 'contact' => 'static_pages#contact', :as => :home_contact
 
 end
